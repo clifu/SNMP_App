ee3f5bc1b5d06dc813180a6e4166bdfc18f5c31c
clifu <d.fronia@o2.pl>
2018-03-02 09:18:12 +0100

    Ready app

---
+++ SnmpServer/SnmpServer/Server.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using SnmpServer;
+
+namespace SnmpServer
+{
+    public class Server
+    {
+        Snmp snmp;
+
+        public Server()
+        {
+            snmp = new Snmp();
+            waitForConnection();
+        }
+
+        public void waitForConnection()
+        {
+            UdpClient udpServer = new UdpClient(11000);
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("192.168.0.100"),11000);
+            Console.WriteLine("Listening...");
+
+            while (true)
+            {
+                var receive = udpServer.Receive(ref endPoint);
+                string time = null;
+                time = DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss.fff", CultureInfo.InvariantCulture);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(time + " Received value is: " + Encoding.ASCII.GetString(receive));
+
+                string value = snmp.get(Encoding.ASCII.GetString(receive))[2];
+                if (value.Equals(""))
+                {
+                    value = "Empty";
+                }
+                time = DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss.fff", CultureInfo.InvariantCulture);
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(time + " Sending value: " + value);
+
+                var client = new UdpClient("192.168.0.101", 11001);
+
+                byte[] msg = Encoding.ASCII.GetBytes(value);
+                int size = Encoding.ASCII.GetByteCount(value);
+                client.Send(msg, size);
+            }
+        }
+    }
+}
---
+++ SnmpServer/SnmpServer/Snmp.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SnmpSharpNet;
+
+namespace SnmpServer
+{
+    class Snmp
+    {
+        private SimpleSnmp _snmp;
+
+        public Snmp()
+        {
+            string host = "127.0.0.1";
+            string community = "public";
+            _snmp = new SimpleSnmp(host, community);
+        }
+
+        public string[] get(string oid)
+        {
+            if (!_snmp.Valid)
+            {
+                return null;
+            }
+            Dictionary<Oid, AsnType> result = _snmp.Get(SnmpVersion.Ver2,new string[] { oid });
+
+            if (result == null)
+            {
+                return null;
+            }
+            else
+            {
+                return readGetResult(result);
+            }
+        }
+
+        private string[] readGetResult(Dictionary<Oid, AsnType> result)
+        {
+            if (result == null)
+                return null;
+            var resultStrings = new string[3];
+            foreach (KeyValuePair<Oid, AsnType> kvp in result)
+            {
+                resultStrings[0] = kvp.Key.ToString();
+                resultStrings[1] = SnmpConstants.GetTypeName(kvp.Value.Type);
+                resultStrings[2] = kvp.Value.ToString();
+            }
+            return resultStrings;
+        }
+
+    }
+}
